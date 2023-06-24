63a93987ca43500d12c9c7f36fe0ddd93b55cfe1
matias2609 <83243327+matias2609@users.noreply.github.com>
2023-06-24 16:06:29 -0300

    Create MisSolicitudes.jsx

---
+++ Frontend-pf/src/components/MisSolicitudes.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const MisSolicitudes = () => {
+  return (
+    <><div>
+      <h3 style={{ color: "#494C4F", textAlign: "left", marginLeft: "1.5vw", marginTop: "-8px" }}><br></br><br></br>
+        Historial de Solicitudes de Trabajo<br></br>
+     </h3>
+    </div><br></br>
+    <div class="col-md-3"
+    style={{ marginLeft:"1.5vw"}}>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title d-flex justify-content-between align-items-center">
+                  Solicitud 1 
+                  <a href="      "></a>
+                </h4>
+                <p>Descripción</p>
+                <form action="      " method="      ">
+                    <input type="hidden" name="_method" value="delete"/>
+                </form>
+            </div>
+        </div>
+     </div>
+     <div class="col-md-3"
+    style={{ marginLeft:"1.5vw"}}><br></br><br></br>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title d-flex justify-content-between align-items-center">
+                  Solicitud 2 
+                  <a href="      "></a>
+                </h4>
+                <p>Descripción</p>
+                <form action="      " method="      ">
+                    <input type="hidden" name="_method" value="delete"/>
+                </form>
+            </div>
+        </div>
+     </div>
+     <div class="col-md-3"
+    style={{ marginLeft:"1.5vw"}}><br></br><br></br>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title d-flex justify-content-between align-items-center">
+                  Solicitud 3 
+                  <a href="      "></a>
+                </h4>
+                <p>Descripción</p>
+                <form action="      " method="      ">
+                    <input type="hidden" name="_method" value="delete"/>
+                </form>
+            </div>
+        </div>
+     </div>
+    <br></br><br></br><br></br></>
+  );
+}
+
+export default MisSolicitudes;
